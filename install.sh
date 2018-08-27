ff6183fb83893f68436cd9da0eb7b5cdbee338ca
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-27 19:09:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir /root/.alqo
 chmod -R 755 /root/.alqo
 GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
-echo -e "rpcuser=alqorpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=256\ndaemon=1\naddnode=89.47.163.2\naddnode=47.92.83.50\naddnode=89.40.15.152\naddnode=80.209.227.35\naddnode=125.253.112.169\naddnode=80.209.230.147\naddnode=194.135.85.198\naddnode=89.40.6.206\naddnode=62.77.155.200\naddnode=47.52.250.198\naddnode=80.209.235.6\naddnode=144.202.119.189\naddnode=207.246.79.142\naddnode=45.32.50.55\naddnode=140.82.51.42\naddnode=172.104.245.54\naddnode=107.191.47.66\naddnode=45.77.86.192\naddnode=89.47.160.79\naddnode=194.135.88.254\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}" > /root/.alqo/alqo.conf
+echo -e "rpcuser=alqorpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=256\ndaemon=1\naddnode=89.47.163.2\naddnode=77.81.234.189\naddnode=212.237.26.159\naddnode=89.40.15.152\naddnode=80.209.227.35\naddnode=125.253.112.169\naddnode=80.209.230.147\naddnode=194.135.85.198\naddnode=89.40.6.206\naddnode=62.77.155.200\naddnode=47.52.250.198\naddnode=80.209.235.6\naddnode=144.202.119.189\naddnode=207.246.79.142\naddnode=45.32.50.55\naddnode=140.82.51.42\naddnode=172.104.245.54\naddnode=107.191.47.66\naddnode=45.77.86.192\naddnode=89.47.160.79\naddnode=194.135.88.254\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}" > /root/.alqo/alqo.conf
 alqod
 sleep 120
 masternodekey=$(alqo-cli masternode genkey)
