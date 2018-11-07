bb8c25b43aa7a0b405dee9a1da236546addb2941
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-11-07 09:24:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt -y update && sudo apt -y install build-essential libssl-dev libdb++-dev && sudo apt -y install libboost-all-dev libcrypto++-dev libqrencode-dev && sudo apt -y install libminiupnpc-dev libgmp-dev libgmp3-dev autoconf && sudo apt -y install autogen automake libtool autotools-dev pkg-config && sudo apt -y install bsdmainutils software-properties-common && sudo apt -y install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev && sudo add-apt-repository ppa:bitcoin/bitcoin -y && sudo apt-get update && sudo apt-get install libdb4.8-dev libdb4.8++-dev -y && sudo apt-get install unzip -y && sudo apt-get install -y pwgen
 cd /usr/local/bin
-wget https://builds.alqo.org/linux/alqo-cli
-wget https://builds.alqo.org/linux/alqod
+wget https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/alqo-cli
+wget https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/alqod
 chmod -R 755 alqod
 chmod -R 755 alqo-cli
 cd
